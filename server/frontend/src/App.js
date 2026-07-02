fb12da5602d0e8e463d396a1c7b61e98b9d313d8
meowzz28 <rogerlim799@gmail.com>
2026-07-02 11:05:23 -0400

    Update React

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,15 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // 1. Import the new component
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      {/* 2. Add the route for the register page */}
+      <Route path="/register" element={<Register />} /> 
     </Routes>
   );
 }
+
 export default App;
